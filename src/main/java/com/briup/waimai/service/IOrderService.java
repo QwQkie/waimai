83d39e55545eb35b1f5fa2f690e503ccc4a63e92
marb <1430109296@qq.com>
2020-01-05 10:22:28 +0800

    mrb

---
+++ src/main/java/com/briup/waimai/service/IOrderService.java
@@ -0,0 +1,14 @@
+package com.briup.waimai.service;
+
+
+import com.briup.waimai.bean.Oder;
+import com.briup.waimai.bean.ex.OderEX;
+
+import java.util.List;
+public interface IOrderService {
+    List<Oder> selectAll() throws RuntimeException;
+    void insert(Oder oder) throws RuntimeException;
+    void deleteById(int id);
+    void updateById(Oder oder);
+    List<OderEX> selectUser(int id);
+}
